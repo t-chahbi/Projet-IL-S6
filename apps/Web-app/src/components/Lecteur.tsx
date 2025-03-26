471457a4efc5471cf1080c2a1071144d88d80f8b
Kaisen00 <ilias.ait-moh@alumni.univ-avignon.fr>
2025-03-26 10:32:55 +0100

    component Lecteur

---
+++ apps/Web-app/src/components/Lecteur.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+export default function Lecteur() {
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/VIDEO_ID"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
---
+++ apps/web-app/src/app/room/[roomId]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Lecteur from '@/components/Lecteur';
+
+export default function page() {
+  return (
+    <>
+      <div>
+        <Lecteur />
+      </div>
+    </>
+  );
+}
