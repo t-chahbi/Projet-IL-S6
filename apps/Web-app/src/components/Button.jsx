f477e9255ee8b5a5b2ea96c747f960ed173c260b
mini <projet>
2025-03-26 10:23:17 +0100

    commit last change

--- apps/Web-app/src/components/Button.jsx
+++ apps/Web-app/src/components/Button.jsx
@@ -1,3 +1,4 @@
 export default function Button({ text }) {
   return <button className="bg-blue-500 text-white p-2">{text}</button>;
 }
+export{Button}
--- apps/Web-app/src/components/component.jsx
+++
@@ -1,4 +0,0 @@
-import Button from "./ui/Button";
-
-
-export { Button };
