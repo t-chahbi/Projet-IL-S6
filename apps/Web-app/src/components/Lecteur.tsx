59edd3cb40934f773eefb04e63cd85a2958aeab2
Kaisen00 <ilias.ait-moh@alumni.univ-avignon.fr>
2025-03-26 11:26:15 +0100

    Ajout Nouveau Lecteur avec video.js

--- apps/Web-app/src/components/Lecteur.tsx
+++ apps/Web-app/src/components/Lecteur.tsx
@@ -1,16 +1,49 @@
-import React from 'react';
-export default function Lecteur() {
+'use client';
+import React, { useEffect, useRef } from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+import 'videojs-youtube';
+
+interface LecteurProps {
+  videoUrl: string;
+}
+
+export default function Lecteur({ videoUrl }: LecteurProps) {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (!videoRef.current) return;
+    const player = videojs(videoRef.current, {
+      controls: true,
+      autoplay: false,
+      preload: 'auto',
+      responsive: true,
+      fluid: true,
+      techOrder: ['youtube'],
+      sources: [
+        {
+          src: videoUrl,
+          type: 'video/youtube',
+        },
+      ],
+    });
+
+    player.on('error', () => {
+      console.error('Erreur lors du chargement de la vidéo.');
+    });
+
+    return () => {
+      if (player) {
+        player.dispose();
+      }
+    };
+  }, [videoUrl]);
+
   return (
     <div>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/VIDEO_ID"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <div data-vjs-player>
+        <video ref={videoRef} className="video-js" />
+      </div>
     </div>
   );
 }
--- apps/web-app/src/app/room/[roomId]/page.tsx
+++ apps/web-app/src/app/room/[roomId]/page.tsx
@@ -1,12 +1,11 @@
+'use client';
 import React from 'react';
 import Lecteur from '@/components/Lecteur';
 
 export default function page() {
   return (
-    <>
-      <div>
-        <Lecteur />
-      </div>
-    </>
+    <div className="flex justify-center items-center h-screen">
+      <Lecteur videoUrl="https://youtu.be/1GtAkWMFnyM?si=gq_G-fHFj3-sC4eB" />
+    </div>
   );
 }
