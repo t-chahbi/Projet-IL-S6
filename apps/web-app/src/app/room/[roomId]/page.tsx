e02fd86eb5740d11be0244e39a732f66cac46c5d
Kaisen00 <ilias.ait-moh@alumni.univ-avignon.fr>
2025-04-02 08:38:55 +0200

    Update page.tsx

--- apps/web-app/src/app/room/[roomId]/page.tsx
+++ apps/web-app/src/app/room/[roomId]/page.tsx
@@ -5,7 +5,7 @@ import Lecteur from '@/components/Lecteur';
 export default function page() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <Lecteur videoUrl="https://youtu.be/1GtAkWMFnyM?si=gq_G-fHFj3-sC4eB" />
+      <Lecteur videoUrl="https://youtu.be/qIPdQmzuX9I?si=aeef7szeLDqV2UGy" />
     </div>
   );
 }
